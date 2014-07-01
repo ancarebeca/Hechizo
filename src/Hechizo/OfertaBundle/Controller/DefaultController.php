c3684958fa938162d57ef04406c3e83ce0e21c95
Rebeca Mora <anca.rebeca@gmail.com>
2014-07-01 21:05:03 +0100

    The first page (index) has been created

--- src/Hechizo/OfertaBundle/Controller/DefaultController.php
+++ src/Hechizo/OfertaBundle/Controller/DefaultController.php
@@ -4,12 +4,24 @@ namespace Hechizo\OfertaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 
 class DefaultController extends Controller
 {
-    public function ayudaAction()
+    public function portadaAction()
     {
-       return $this->render('OfertaBundle:Default:index.html.twig');
+        $em = $this -> getDoctrine()->getManager();
+        $ciudad = $em -> getRepository('CiudadBundle:Ciudad')->findByNombre('Madrid');
+        $today = new \DateTime('2014-07-01 23:59:59');
+        $oferta = $em -> getRepository('OfertaBundle:Oferta')->findOneBy(array(
+            'ciudad' => $ciudad,
+            'fecha_publicacion' => $today
+    ));
+
+        return $this->render(
+            'OfertaBundle:Default:portada.html.twig',
+            array('oferta' => $oferta)
+        );
     }
 }
